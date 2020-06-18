8b2acce49e7b830186eda1b685d8c1a5acf329eb
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-06-17 22:28:21 -0400

    Added Postgres connection

--- postgres_scripts/10_insert_permisos_table.sql
+++ postgres_scripts/10_insert_permisos_table.sql
@@ -1,2 +1,6 @@
 INSERT INTO permisos(id_persona, region, comuna, direccion, motivo)
-VALUES ('198189790', 'Region Metropolitana', 'Quilicura', 'Los Armenios #0270', 'A1')
+VALUES
+    ('198189790', 'Metropolitana', 'Quilicura', 'Los Armenios #0270', 'A1'),
+    ('196621903', 'Coquimbo', 'La Serena', 'El Litre #2162', 'A1'),
+    ('194202868', 'Metropolitana', 'Pedro Aguirrez Cerda', 'Nueva Dos #5169', 'A1'),
+    ('195463964', 'Metropolitana', 'Quilicura', 'Av. Manuel Antonio Matta #0280', 'A1');
