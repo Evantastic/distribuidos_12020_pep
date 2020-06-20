2cfa85c3dfed7199040cb7c3d16d3b51faaec744
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-06-19 23:54:17 -0400

    Added Validation API

--- backend/src/routes/permisos.js
+++ backend/src/routes/permisos.js
@@ -9,8 +9,11 @@ module.exports = router;
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params
-    const { rows } = await db.query('SELECT * FROM permisos WHERE id_persona = $1',  [ id ]);
-    res.status(200).send(rows[0]);
+    const { rows } = await db.query('SELECT * FROM permisos WHERE id = $1',  [ id ]);
+    const now = DateTime.local().setZone('America/Santiago').toFormat('y-LL-dd HH:mm');
+    const limit = DateTime.fromFormat(rows[0].valido_hasta,'y-LL-dd HH:mm').toFormat('y-LL-dd HH:mm');
+    const is_valid = now < limit;
+    res.status(200).send({ es_valido: is_valid, hora_actual: now, hora_limite: limit});
 });
 
 router.post('/', async (req, res) => {
