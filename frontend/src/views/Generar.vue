aa01b06bebb05b4cc28a7c04e2766ad5f22358c9
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-06-22 21:51:53 -0400

    Fixed coloring

--- frontend/src/views/Generar.vue
+++ frontend/src/views/Generar.vue
@@ -10,7 +10,7 @@
     <div class="field">
       <label class="label">RUT/Pasaporte</label>
       <div class="control">
-        <input class="input" type="text" placeholder="" />
+        <input class="input is-dark is-rounded" type="text" placeholder="" />
       </div>
       <p class="help">No usar puntos ni guiones</p>
     </div>
@@ -18,21 +18,21 @@
     <div class="field">
       <label class="label">Region</label>
       <div class="control">
-        <input class="input" type="text" placeholder="" />
+        <input class="input is-dark is-rounded" type="text" placeholder="" />
       </div>
     </div>
 
     <div class="field">
       <label class="label">Comuna</label>
       <div class="control">
-        <input class="input" type="text" placeholder="" />
+        <input class="input is-dark is-rounded" type="text" placeholder="" />
       </div>
     </div>
 
     <div class="field">
       <label class="label">Direccion</label>
       <div class="control">
-        <input class="input" type="text" placeholder="" />
+        <input class="input is-dark is-rounded" type="text" placeholder="" />
       </div>
     </div>
 
