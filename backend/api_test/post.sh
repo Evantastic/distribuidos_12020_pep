0b122cf598534de846d0a6b35ee0a8ce19637f28
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-09-10 14:14:51 -0300

    Comendazo Pep 2
    Se indico el comando necesario para poder ejecutar beackend de forma
    autonoma

--- backend/api_test/post.sh
+++ backend/api_test/post.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-http -f POST localhost:3000/api/permisos/ id_persona=11251970k region=Metropolitana comuna=Quilicura direccion='Los Armenios #0270' motivo=2
-http -f POST localhost:3000/api/permisos/ id_persona=197828900 region=Metropolitana comuna=Quilicura direccion='Pje. Del Arado #014 Villa Lo Matta' motivo=1
-http -f POST localhost:3000/api/permisos/ id_persona=197842121 region=Metropolitana comuna=Quilicura direccion='Pje 7 Raul Silva Henriquez' motivo=7
+http -f POST localhost:3000/api/permisos/ id_persona=11251970k region=Metropolitana comuna=Quilicura direccion='Los Armenios #0270' ventana=2
+http -f POST localhost:3000/api/permisos/ id_persona=197828900 region=Metropolitana comuna=Quilicura direccion='Pje. Del Arado #014 Villa Lo Matta' ventana=1
+http -f POST localhost:3000/api/permisos/ id_persona=197842121 region=Metropolitana comuna=Quilicura direccion='Pje 7 Raul Silva Henriquez' ventana=7
