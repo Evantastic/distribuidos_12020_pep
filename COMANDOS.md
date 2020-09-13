4b6f091dd50f27255ea643475334cd12d7deb9e1
Alan Barahona Ruiz <barahonaruiz.alan@gmail.com>
2020-09-13 20:21:38 -0300

    Limpieza

--- COMANDOS.md
+++
@@ -1,7 +0,0 @@
-# Backend
-
-Para ejecutar el backend, y que este pueda comunicarse con la base de datos que se encuentra en localhost:5432, usar el siguiente comando.
-```
-docker run --rm --name backend --network="host" --env-file ../backend.env backend:prod
-```
-La informacion que debe ir contenida en `env-file` esta especificado en el README. Para que esto funcione, la base de datos de google debe estar a traves del proxy.
--- postgres_scripts/10_insert_permisos_table.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO permisos(id_persona, region, comuna, direccion, motivo, hora_emision, valido_hasta)
-VALUES
-    ('198189790', 'Metropolitana', 'Quilicura', 'Los Armenios #0270', 1, '2020-05-14 16:20', '2020-05-14 19:20'),
-    ('196621903', 'Coquimbo', 'La Serena', 'El Litre #2162', 2, '2020-05-15 14:20', '2020-05-15 17:20'),
-    ('194202868', 'Metropolitana', 'Pedro Aguirrez Cerda', 'Nueva Dos #5169', 3, '2020-06-10 18:00',  '2020-06-10 20:00'),
-    ('195463964', 'Metropolitana', 'Quilicura', 'Av. Manuel Antonio Matta #0280', 4, '2020-06-15 19:21',  '2020-06-15 20:21');
